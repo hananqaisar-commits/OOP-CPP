f3e71a89bff9a953e7627b56d2e0df7fb39aba22
Hanan Qaisar <hananqaisar316@gmail.com>
2026-01-30 22:29:00 +0500

    Add card insertion,Modify PIN verification, and basic transactions

--- ATM-simulation-oop.cpp
+++ ATM-simulation-oop.cpp
@@ -1,5 +1,13 @@
+// My programme flow:
+//   Insert card
+//  ️ Enter PIN (once)
+//  ️ If PIN correct → show menu
+//   Perform multiple actions
+//   Logout
+
 #include <iostream>
 #include <string>
+#include <windows.h>
 using namespace std;
 class BankAccount
 {
@@ -24,31 +32,43 @@ public:
     void display();
     void choice(int n);
     bool checkPin();
+    int insertCard();
 };
 void BankAccount ::deposit(int deposit)
 {
-
-    if (deposit < 0)
+    char action;
+    cout << "Enter cash in right top area\n";
+    cout << "Enter d after done: ";
+    cin >> action;
+    if (action == 'D' || action == 'd')
     {
-        cout << "Sorry! This amount is not deposited" << endl;
+        cout << "Processing, please wait..." << endl;
+        Sleep(2000);
+        if (deposit < 0)
+        {
+            cout << "Sorry! This amount is not deposited" << endl;
+        }
+        else if (deposit > 0)
+        {
+            balance += deposit;
+            cout << "You deposit " << deposit << " rupees sucessfully" << endl;
+        }
+        else
+            cout << "Invalid" << endl;
     }
-    else if (deposit > 0)
-    {
-        balance += deposit;
-        cout << "You deposit " << deposit << " rupees sucessfully" << endl;
-    }
-    else
-        cout << "Invalid" << endl;
 }
 
 void BankAccount ::withdraw(int withdraw)
 {
+    cout << "Processing, please wait..." << endl;
+    Sleep(2000);
     if (withdraw > balance)
     {
         cout << "Sorry! " << withdraw << " amount is not in your account" << endl;
     }
     else if (withdraw < balance && withdraw > 0 || withdraw == balance)
     {
+        cout<<"Take yout cash\n";
         cout << withdraw << " rupees is withdraw sucessfully" << endl;
         balance -= withdraw;
     }
@@ -86,15 +106,18 @@ bool BankAccount ::checkPin()
     while (chance > 0)
     {
         cin >> enteredpin;
+        cout << "Verifying PIN, please wait..." << endl;
+        Sleep(2000); // sleep for 2000 milisecond it is in window.h library
+
         if (enteredpin == pin)
         {
-            cout << "Correct pin" << endl;
+            cout << "Correct pin\nAccess granted" << endl;
             return true;
         }
         else
         {
             cout << "Wrong pin " << --chance << " more chance(s) left" << endl
-                 << " Otherwise permission will be denied\nTry again: " << endl;
+                 << "Otherwise permission will be denied\nTry again: " << endl;
         }
     }
     cout << "Sorry permission denied\n";
@@ -108,7 +131,12 @@ void BankAccount ::choice(int n)
     switch (n)
     {
     case 0:
+        cout << "Card is coming out, please wait..." << endl;
+        Sleep(3000);
+        cout << "Take your card\n";
+        Sleep(1000);
         cout << "logout sucessfull" << endl;
+        cout << "Thanks for using me" << endl;
         break;
     case 1:
     {
@@ -138,39 +166,66 @@ void BankAccount ::choice(int n)
         cout << "Enter valid choice" << endl;
     }
 }
-
+int BankAccount ::insertCard()
+{
+    int insert;
+    cout << "=====Welcome to ATM=====\nInsert your card" << endl;
+    cout << "1 for insert card\n0 for nothing" << endl;
+    cin >> insert;
+    if (insert == 1)
+    {
+        cout << "Processing your card, please wait..." << endl;
+        Sleep(3000);
+        return 1;
+    }
+    else if (insert == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        cout << "Invalid Card\n";
+        return 0;
+    }
+}
 int main()
 {
     int choice;
     BankAccount account1(1199, "Hanan Qaisar", 0, 1493, 3);
-    cout << "\n====ATM DISPLAY====\n";
-    cout << "Choose one option:" << endl
-         << "1. Deposit" << endl
-         << "2. Withdraw" << endl
-         << "3. Check Balabnce" << endl
-         << "4. Display detail" << endl
-         << "0. End" << endl;
-
-    do
+    if (account1.insertCard())
     {
-        cin >> choice;
-        if (choice < 0 || choice > 5)
-        {
-            cout << "Enter valid Choice" << endl;
-        }
-        else if ((!account1.checkPin()))
+        if (!account1.checkPin())
         {
             return 0;
         }
         else
         {
-            account1.choice(choice);
-            if (choice != 0)
-            {
-                cout << "Choose next option:" << endl;
-            }
-        }
-    } while (choice != 0);
+            cout << "\n====ATM DISPLAY====\n";
+            cout << "Choose one option:" << endl
+                 << "1. Deposit" << endl
+                 << "2. Withdraw" << endl
+                 << "3. Check Balabnce" << endl
+                 << "4. Display detail" << endl
+                 << "0. End" << endl;
 
-    return 0;
+            do
+            {
+                cin >> choice;
+                if (choice < 0 || choice > 5)
+                {
+                    cout << "Enter valid Choice" << endl;
+                }
+                else
+                {
+                    account1.choice(choice);
+                    if (choice != 0)
+                    {
+                        cout << "Choose next option:" << endl;
+                    }
+                }
+            } while (choice != 0);
+        }
+
+        return 0;
+    }
 }

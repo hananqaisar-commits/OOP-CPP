adcc74bc9d8753233e19d2227199d2991ba9b0cd
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-04 22:58:58 +0500

    Add more options but bugs are created

--- TollBooth.cpp
+++ TollBooth.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 class TollBooth
 {
+    string shift;
+    string collector_name;
+    double totalcash;
+
 public:
     unsigned int totalcars;
-    double totalcash;
+    string type; // vehcle type
 
     TollBooth()
     {
@@ -14,12 +18,15 @@ public:
     }
     void payingcar();
     void nopaycar();
-    void Display();
+    void report();
+    void input();
+    void vehcleType();
+    void Rates_chart();
 };
 
 void TollBooth ::payingcar()
 {
-    totalcash += 0.50;
+
     totalcars++;
 }
 void TollBooth ::nopaycar()
@@ -27,39 +34,77 @@ void TollBooth ::nopaycar()
     totalcars++;
     totalcash = totalcash; // remain same
 }
-void TollBooth ::Display()
+
+void TollBooth ::report()
 {
     cout << "\n====== Toll Booth Report ======\n";
-    cout << "Total cars: " << totalcars << endl;
+    cout << "Collector name: : " << collector_name << endl;
+    cout << "Shift: : " << shift << endl;
     cout << "Total cash: " << totalcash << endl;
+    cout << "Total cars: " << totalcars << endl;
+    cout << "Average tax on every car: " << totalcash / totalcars << endl;
+}
+void TollBooth ::vehcleType()
+{
+
+    cout << "Enter vechile (e.g: car, truck, bus): ";
+    cin >> type;
+}
+void TollBooth ::Rates_chart()
+{
+    cout << "Bike--------->0.00 rs/-\n";
+    cout << "Car--------->0.35 rs/-\n";
+    cout << "Vagon--------->0.35 rs/-\n";
+    cout << "Bus--------->0.50 rs/-\n";
+    cout << "Truck--------->0.80 rs/-\n";
+    cout << "Others--------->0.35 rs/-\n";
+}
+void TollBooth ::input()
+{
+    char choice;
+    cin >> choice;
+    vehcleType();
+
+    do
+    {
+
+        if (type == "car" || type == "Car" || type == "CAR")
+        {
+            totalcash = 0.35;
+        }
+        else if (type == "bus" || type == "Bus" || type == "BUS")
+        {
+            totalcash == 0.50;
+        }
+        else if (type == "truck" || type == "Truck" || type == "TRUCK")
+        {
+            totalcash == 0.80;
+        }
+        else if (type == "bike" || type == "Bike" || type == "BIKE")
+        {
+            totalcash == 0;
+        }
+        else if (type == "vagon" || type == "Vagon" || type == "VAGON")
+        {
+            totalcash = 0.45;
+        }
+        else
+        {
+            totalcash = 0.10;
+        }
+
+        payingcar();
+
+        cout << "Enter next car: " << endl;
+        cin >> choice;
+        
+    }
+    while (choice != 'e' && choice != 'E');
 }
 int main()
 {
     TollBooth tool;
-    char choice;
-    cout << "Enter choice " << endl
-         << "P = count a paying car" << endl
-         <<
-
-        "N = count a non-paying car" << endl
-         <<
-
-        "E for exit program and show Report" << endl;
-    cin >> choice;
-    do
-    {
-        if (choice == 'P' || choice == 'p')
-        {
-            tool.payingcar();
-        }
-        else if (choice == 'N' || choice == 'n')
-        {
-            tool.nopaycar();
-        }
-        cout << "Enter next car: " << endl;
-        cin >> choice;
-    } while (choice != 'e' && choice != 'E');
-    tool.Display();
+    tool.input();
 
     return 0;
 }

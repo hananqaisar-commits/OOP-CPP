ef7a64d9a6c014c6da72330d143365b278dc3e30
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-04 22:20:48 +0500

    Add only shallow copy

--- deepCopy & shallowCopy.cpp
+++ deepCopy & shallowCopy.cpp
@@ -6,9 +6,9 @@ class Student
 {
     int id;
     string name;
-    int *marks; // it create pointer which is pointing in memory randomly
 
 public:
+    int *marks; // it create pointer which is pointing in memory randomly
     Student(int id, string name, int markptr)
     {
         this->id = id;
@@ -29,17 +29,17 @@ void Student ::display()
     cout << "===Details===\n";
     cout << "Name is: " << name << endl;
     cout << "ID is: " << id << endl;
-    cout << "Marks is: " << marks << endl;
+    cout << "Marks is: " << *marks << endl;//here i am accessing valu of marks by *marks
 }
 
 int main()
 {
-    
+
     Student s1(187, "Hanan Qaisar", 90);
     Student s2(s1);
     s1.display();
+    (*s2.marks) = 78;//to acces *marsk here it must be declared as public in class
     s2.display();
 
-    
     return 0;
 }

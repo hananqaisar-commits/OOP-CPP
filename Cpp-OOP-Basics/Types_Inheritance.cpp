f4095b958b8bc910c01949cfa8d81003280ad3c3
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-09 22:59:26 +0500

    Create constructor types

--- Cpp-OOP-Basics/Types_Inheritance.cpp
+++ Cpp-OOP-Basics/Types_Inheritance.cpp
@@ -1,15 +1,51 @@
 // Mostly 5 types of inheritance:
-    // 1:Single Inheritance e.g: class B : public A { ... };
-    // 2:Multiple Inheritance e.g: class C : public A, public B { ... };
-    // 3:Multilevel Inheritance e.g: Class B: Puclic A, class C : Public B (B is inherited from A, and C is inherited from B.)
-    // 4:Hierarchical Inheritance e.g: B : A, C : A (both B and C is inherited by A)
-    // 5:Hybrid Inheritance e.g: A combination of two or more types of inheritance (like multiple + multilevel).
+// 1:Single Inheritance e.g: class B : public A { ... };
+// 2:Multiple Inheritance e.g: class C : public A, public B { ... };
+// 3:Multilevel Inheritance e.g: Class B: Puclic A, class C : Public B (B is inherited from A, and C is inherited from B.)
+// 4:Hierarchical Inheritance e.g: B : A, C : A (both B and C is inherited by A)
+// 5:Hybrid Inheritance e.g: A combination of two or more types of inheritance (like multiple + multilevel).
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    
+class Person
+{
+public:
+    string name;
+    int age;
+    Person(int age, string name)
+    {
+        this->age = age;
+        this->name = name;
+    }
+};
+
+class Student : public Person
+{
+public:
+    string rollNo;
+    string schoolName;
+    Student(int age, string name, int rollNo, string schoolNme) : Person(age, schoolName)
+    {
+        this->rollNo = rollNo;
+        this->schoolName = schoolName;
+    }
+};
+class Matric : public Person, public Student
+{
+    string registration_Number;
+
+public:
+    string board;
+    Matric(int age, string name, string rollNo, string schoolName, string rn, string board) : Person(age, name, rollNo, schoolName)
+    {
+        rn = registration_Number;
+        this->board = board;
+    }
+};
+int main()
+{
+
     return 0;
 }
